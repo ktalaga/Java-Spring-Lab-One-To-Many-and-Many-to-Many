71e0d6265b25d7d55f731d2797b6c5b300a3e60e
Andrew Sharkey <sharkand@hotmail.com>
2021-06-01 11:40:23 +0100

    Employee and Department test passing, H2 working.

--- src/test/java/com/example/company/company/CompanyApplicationTests.java
+++ src/test/java/com/example/company/company/CompanyApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.company.company;
 
+import com.example.company.company.models.Department;
+import com.example.company.company.models.Employee;
+import com.example.company.company.repositories.DepartmentRepository;
+import com.example.company.company.repositories.EmployeeRepository;
+import com.example.company.company.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void createEmployeeAndDepartment() {
+		Department department = new Department("Back End");
+		departmentRepository.save(department);
+		Employee employee1 = new Employee("Konrad", "Talaga", 99, department);
+		employeeRepository.save(employee1);
+	}
 }
